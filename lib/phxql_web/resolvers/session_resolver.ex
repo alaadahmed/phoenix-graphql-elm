6e61d6d61cd60c566462cda61ac95b5973b5dac6
AlaaEldin Ahmed <alaadahmed@gmail.com>
2019-06-23 01:56:42 +0200

    implementing session and user types and resolvers

--- lib/phxql_web/resolvers/session_resolver.ex
+++ lib/phxql_web/resolvers/session_resolver.ex
@@ -1,2 +1,11 @@
 defmodule PhxQLWeb.Resolvers.SessionResolver do
+  alias PhxQL.Accounts.Auth
+  alias PhxQLWeb.Auth.Guardian, as: Guard
+
+  def login_user(_, %{input: %{email: email, password: password}}, _) do
+    with {:ok, user} <- Auth.sign_in(email, password),
+         {:ok, jwt_token, _} <- Guard.encode_and_sign(user) do
+      {:ok, %{token: jwt_token, user: user}}
+    end
+  end
 end
--- lib/phxql_web/resolvers/user_resolver.ex
+++ lib/phxql_web/resolvers/user_resolver.ex
@@ -1,2 +1,11 @@
 defmodule PhxQLWeb.Resolvers.UserResolver do
+  alias PhxQL.Accounts
+
+  def users(_, _, %{context: context}) do
+    {:ok, Accounts.list_users()}
+  end
+
+  def register_user(_, %{input: params}, _) do
+    Accounts.create_user(params)
+  end
 end
